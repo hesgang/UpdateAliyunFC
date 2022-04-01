22b4b8d82323b777c42a9f8f968260206a3accc2
hesgang <1034680772@qq.com>
2022-04-01 18:50:18 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ set -e
 
 pip3 install -r /updateFC/requirements.txt
 
-python3 /updateFC/UpdateAliyunFC.py --access_key_id "${INPUT_ACCESS_KEY}" \
---access_key_secret "${INPUT_ACCESS_KEY_SECRET}" \
---account_ID "${INPUT_ACCOUNT_ID}" \
---server_name "${INPUT_SERVER_NAME}" \
---function_name "${INPUT_FUNCTION_NAME}" \
+python3 /updateFC/UpdateAliyunFC.py --access-key-id "${INPUT_ACCESS_KEY}" \
+--access-key-secret "${INPUT_ACCESS_KEY_SECRET}" \
+--account-ID "${INPUT_ACCOUNT_ID}" \
+--server-name "${INPUT_SERVER_NAME}" \
+--function-name "${INPUT_FUNCTION_NAME}" \
 --region "${INPUT_REGION}" 
