3eb26e49257246ecfec986c3612b00db126a8f84
hesgang <1034680772@qq.com>
2022-04-01 20:20:28 +0800

    test

--- updateFC/UpdateAliyunFC.py
+++ updateFC/UpdateAliyunFC.py
@@ -70,8 +70,8 @@ class UpdateFC:
         return FC_Open20210406Client(config)
 
     @staticmethod
-    def main(_ak, _sk, _sever_name, _func_name, zipcode):
-        client = UpdateFC.create_client(_ak, _sk)
+    def main(_ak, _sk, _accountid, _sever_name, _func_name, zipcode):
+        client = UpdateFC.create_client(_ak, _accountid, _sk)
         update_function_headers = fc__open_20210406_models.UpdateFunctionHeaders()
         update_function_request = fc__open_20210406_models.UpdateFunctionRequest(code=zipcode)
         resp = client.update_function_with_options(_sever_name, _func_name, update_function_request, update_function_headers, util_models.RuntimeOptions())
@@ -99,5 +99,5 @@ if __name__ == '__main__':
     #     f.close()
     #     print(encoded)
     zipfile = fc__open_20210406_models.Code(zip_file=file)
-    UpdateFC.main(args.access_key_id, args.access_key_secret, args.server_name, args.function_name, zipfile)
+    UpdateFC.main(args.access_key_id, args.access_key_secret, args.account_ID, args.server_name, args.function_name, zipfile)
 
