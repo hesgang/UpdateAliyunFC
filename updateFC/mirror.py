5182bb4773ff5f795faf584ef9708b16dfe0f50f
hesgang <1034680772@qq.com>
2022-04-04 21:12:08 +0800

    update:加入仓库自动根据仓库进行推送

--- updateFC/mirror.py
+++ updateFC/mirror.py
@@ -21,13 +21,32 @@ class Mirror(object):
         self.clone_repo = """https://github.com/{}.git""".format(repo)
 
     @retry(wait=wait_exponential(), reraise=True, stop=stop_after_attempt(3))
+    def download(self):
+        print("Downloading...")
+        try:
+            r = requests.get(self.master_url)
+            if r.content == b'404: Not Found':
+                r = requests.get(self.main_url)
+            name = 'cache.zip'
+            with open(name, 'wb') as f:
+                f.write(r.content)
+                f.close()
+        except Exception as e:
+            print(e)
+            self._clone()
+
+    @retry(wait=wait_exponential(), reraise=True, stop=stop_after_attempt(3))
     def clone(self):
         print('cloning to local...')
         mygit = git.cmd.Git(os.getcwd())
         mygit.clone(git.cmd.Git.polish_url(self.clone_repo))
 
     def in_zip(self):
-        self.clone()
+        try:
+            self.clone()
+        except Exception as e:
+            print(e)
+
         os.chdir(self.repo_name)
         with zipfile.ZipFile('../cache.zip', 'w') as target:
             for i in os.walk("."):
